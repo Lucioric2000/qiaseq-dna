3846b17858cc3c5190fe4a152446fd51050a7736
lucioric <lucioric2000@hotmail.com>
2018-11-01 12:03:33 -0600

    Update install_qiaseq_dna.bash

--- install_qiaseq_dna.bash
+++ install_qiaseq_dna.bash
@@ -144,7 +144,7 @@ ls ${srv_qiagen}/data/genome/ucsc.hg19.fa.pac.md5 &>/dev/null && echo found bwa 
          https://storage.googleapis.com/qiaseq-dna/data/genome/ucsc.hg19.fa.gz -P ${srv_qiagen}/data/genome/
     cd ${srv_qiagen}/data/genome && \
         gunzip ucsc.hg19.fa.gz  && \
-        ${CONDA_PREFIX}/bin/samtools faidx ${srv_qiagen}/data/genome/ucsc.hg19.fa && \ 
+        ${CONDA_PREFIX}/bin/samtools faidx ${srv_qiagen}/data/genome/ucsc.hg19.fa && \
         ${CONDA_PREFIX}/bin/bwa index ${srv_qiagen}/data/genome/ucsc.hg19.fa
         md5sum -b ${srv_qiagen}/data/genome/ucsc.hg19.fa.pac > ${srv_qiagen}/data/genome/ucsc.hg19.fa.pac.md5 )
 cd ${srv_qiagen}/qiaseq-dna
